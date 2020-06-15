937f1b8cda4dcea8a5d0f28bd030db62eace153c
Eduardo Maciel Sanchez <54561697+DesolateElf-dev@users.noreply.github.com>
2020-06-15 00:21:38 -0300

    Add files via upload
    Primeiro upload

---
+++ src/controller/RelatorioContoller.java
@@ -0,0 +1,37 @@
+package controller;
+
+import java.io.IOException;
+
+public class RelatorioContoller implements IRelatorioController {
+
+	public RelatorioContoller() {
+		super();
+	}
+
+	@Override
+	public void readDir(String path) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void createFile(String path, String name) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void readFile(String path, String name) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void openFile(String path, String name) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+}
