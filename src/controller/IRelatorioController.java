65fa278a45d2d28641208b58b0237f4db1bc9c75
DesolateElf-dev <54561697+DesolateElf-dev@users.noreply.github.com>
2020-06-15 18:43:14 -0300

    update

--- src/controller/IRelatorioController.java
+++ src/controller/IRelatorioController.java
@@ -3,10 +3,8 @@ package controller;
 import java.io.IOException;
 
 public interface IRelatorioController {
-	
-	public void readDir(String path) throws IOException;
-	
-	public void createFile(String path, String name) throws IOException;
+
+	public void createFile(String path, String name, String name2) throws IOException;
 	
 	public void readFile(String path, String name) throws IOException;
 	
--- src/view/Principal.java
+++ src/view/Principal.java
@@ -1,10 +1,62 @@
 package view;
 
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
+import controller.IRelatorioController;
+import controller.RelatorioController;
+
+//Pegar o arquivo relatório.txt, colocar em um diretório que o permita ser lido e:
+//faça a leitura e coloque seu conteúdo em um arquivo CSV, para que seja lido pelo Excel.
+
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		IRelatorioController arqCont = new RelatorioController();
+		String path = "C:\\temp"; //diretório
+		String name = "relatorio.txt"; //arquivo lido .txt
+		String name2 = "relatorio.csv"; //arquivo criado .csv
+		
+		try {
+			int opc=0;
 
+			while(opc!=9) {
+				opc = Integer.parseInt(JOptionPane.showInputDialog(
+						"1- Lê arquivo"+
+						"\n2- Cria relatorio.csv" +
+						"\n3 - Abre arquivo no excel"+
+						"\n9 - Finalizar"));
+				switch (opc){
+					case 1: {
+		    			int opc2 = Integer.parseInt(JOptionPane.showInputDialog(null, "1- relatório.txt \n2- Relatório.csv",
+		    					"Entrada de inteiro", JOptionPane.INFORMATION_MESSAGE));
+		    			if (opc2 == 1) {
+		    				arqCont.readFile(path, name);
+		    			} else {
+		    				if (opc2 ==2) {
+		    					arqCont.readFile(path, name2);
+		    				} else {
+		    					JOptionPane.showMessageDialog(null,"Opção Inválida");
+		    				}
+		    			}
+		    		}
+						break;
+		    		case 2: arqCont.createFile(path, name, name2);
+		    			break;
+		    		case 3: arqCont.openFile(path, name2);
+		    			break;
+		    		case 9: {
+		    			System.out.println("Programa Finalizado");
+		    			System.exit(0);
+		    		}
+		    		default: JOptionPane.showMessageDialog(null,"Inválido");
+				}
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-
 }
